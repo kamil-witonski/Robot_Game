d65f09d594b62c5c7d64ef4f6cf416a68d5b1dd2
Kamil <kamil.witonski@o2.pl>
2017-10-30 19:51:22 +0000

    add rpg rocket script

---
+++ robot/Assets/Scripts/RPG_Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPG_Rocket : MonoBehaviour {
+
+	public AudioClip explosionSound;
+
+	void Explosion()
+	{
+		var rig = GetComponent<Rigidbody>();
+		rig.isKinematic = true;
+
+		var rend = GetComponent<Renderer>();
+		rend.enabled = false;
+
+		AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+
+		//particl system
+		var explosion = GetComponent<ParticleSystem>();
+		explosion.Play();
+		Destroy(gameObject, explosion.duration);
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		Debug.Log("explosion");
+		Explosion();
+
+
+	}
+}
