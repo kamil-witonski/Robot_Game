3173e0c9fe19cf1012087caa2305f4176cd0d1e9
Kamil <kamil.witonski@o2.pl>
2017-11-03 23:50:37 +0000

    update bullet script to use updated player and enemy class

---
+++ robot/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public int damageValue;
+	public string targetTag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == targetTag) {
+			Debug.Log ("Hit enemy");
+
+			Entity enemy = collision.gameObject.GetComponent<Entity> ();
+
+			enemy.TakeDamage (damageValue);
+		}
+	}
+}
