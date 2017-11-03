d1033c4d546d009f3d8b71be18c7983e9fb7a72c
Kamil <kamil.witonski@o2.pl>
2017-11-03 23:49:10 +0000

    add simple point at script

---
+++ robot/Assets/Scripts/PointAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAt : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt (target.position);
+
+		Vector3 relPos = target.position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation (relPos);
+
+		transform.rotation = rotation;
+	}
+}
