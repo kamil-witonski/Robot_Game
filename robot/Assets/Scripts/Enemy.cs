333d8e1a719a22ef29994c1cf8e09fbd8ff0e597
Kamil <kamil.witonski@o2.pl>
2017-11-03 23:48:16 +0000

    update player and enemy to extend from entity class

---
+++ robot/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	public override void Die()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ robot/Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	public int health = 100;
+
+	public virtual void Die(){}
+
+	public void TakeDamage(int damagePoints) {
+		health -= damagePoints;
+
+
+		if (health <= 0) {
+			Die();
+		}
+	}
+}
