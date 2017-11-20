501164c545a786c35114a2d8ca6a0446cf506932
Kamil <kamil.witonski@o2.pl>
2017-11-20 22:49:17 +0000

    add radio comms manager
    plays audio in sequence

--- robot/Assets/Scripts/RadioManager.cs
+++ robot/Assets/Scripts/RadioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadioManager : MonoBehaviour {
 
@@ -12,6 +13,9 @@ public class RadioManager : MonoBehaviour {
 
 	public bool playAudio = false;
 
+	public GameObject uiImage;
+	public GameObject objectives;
+
 	public RadioStation currStation {
 		get { return radioStations [currStationIndex];}
 	}
@@ -22,6 +26,9 @@ public class RadioManager : MonoBehaviour {
 
 		//set to no radio at start
 		currStationIndex = 0;
+		currStation.currTrackIndex = 0;
+		// uiImage.enabled = false;
+		enableImages(uiImage, false);
 		// Audio.loop = currStation.tracks [currStation.currTrackIndex].loop;
 
 		// Audio.clip = currStation.tracks [currStation.currTrackIndex].Audio;
@@ -31,10 +38,6 @@ public class RadioManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.H)) {
-			playAudio = true;
-		}
-
 		if(!playAudio) {
 			return;
 		}
@@ -42,17 +45,21 @@ public class RadioManager : MonoBehaviour {
 		//check if song is playing
 		if (!Audio.isPlaying) {
 			
-			if (currStation.loopAtEnd && currStation.tracks.IndexOf (currStation.tracks [currStation.currTrackIndex]) == currStation.tracks.Count - 1 && !currStation.tracks [currStation.currTrackIndex].loop) {
-				currStation.currTrackIndex = 0;
+			// if (currStation.loopAtEnd && currStation.tracks.IndexOf (currStation.tracks [currStation.currTrackIndex]) == currStation.tracks.Count - 1 && !currStation.tracks [currStation.currTrackIndex].loop) {
+				// currStation.currTrackIndex = 0;
 				//set the audio object to the current song on the station
-				Audio.loop = currStation.tracks [currStation.currTrackIndex].loop;
-				Audio.clip = currStation.tracks [currStation.currTrackIndex].Audio;
+				// Audio.loop = currStation.tracks [currStation.currTrackIndex].loop;
+				// Audio.clip = currStation.tracks [currStation.currTrackIndex].Audio;
 
-				Audio.Play ();
-			} else {
+				// Audio.Play ();
+			// } else {
 
 				if (currStation.tracks.IndexOf (currStation.tracks [currStation.currTrackIndex]) == currStation.tracks.Count - 1) {
 					Audio.Stop ();
+					// uiImage.enabled = false;
+					enableImages(uiImage, false);
+					enableImages(objectives, true);
+					playAudio = false;
 				} else {
 					if (!currStation.tracks [currStation.currTrackIndex].loop) {
 						currStation.currTrackIndex++;
@@ -64,7 +71,7 @@ public class RadioManager : MonoBehaviour {
 					Audio.clip = currStation.tracks [currStation.currTrackIndex].Audio;
 					Audio.Play();
 				}
-			}
+			// }
 		}
 
 		if (Input.GetKeyDown (KeyCode.KeypadPlus) && Audio.volume < 1) {
@@ -87,6 +94,24 @@ public class RadioManager : MonoBehaviour {
 		// 	Audio.Stop ();
 		// }
 	}
+
+	void OnTriggerEnter(Collider coll) {
+		playAudio = true;
+		// uiImage.enabled = true;
+		enableImages(uiImage, true);
+	}
+
+	void enableImages(GameObject obj, bool enable) {
+		obj.gameObject.SetActive(enable);
+		foreach(Transform child in obj.transform) {
+			Debug.Log(child);
+			if(enable) {
+				child.gameObject.SetActive(true);
+			} else {
+				child.gameObject.SetActive(false);
+			}
+		}
+	}
 }
 
 [System.Serializable]
@@ -97,7 +122,7 @@ public class RadioStation
 	public List<RadioStationTrack> tracks = new List<RadioStationTrack>();
 
 	public bool loopAtEnd = true;
-	public int currTrackIndex;
+	public int currTrackIndex = 0;
 
 	[System.Serializable]
 	public class RadioStationTrack
