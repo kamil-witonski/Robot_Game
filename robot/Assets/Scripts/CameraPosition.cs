0773f5b2269d157b42c048f788fd7970ccdc1d07
Kamil <kamil.witonski@o2.pl>
2018-02-07 19:28:34 +0000

    add script to switch camera positions

---
+++ robot/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    public int currentCam = 0;
+
+    public Vector3[] cameraPositions = new[] { new Vector3(10.3f, 4.15f, 0f), new Vector3(0.78f, 1.41f, 0f) };
+
+    private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            currentCam++;
+
+            if(currentCam >= cameraPositions.Length)
+            {
+                currentCam = 0;
+            }
+
+            mainCam.transform.localPosition = cameraPositions[currentCam];
+        }
+    }
+}
