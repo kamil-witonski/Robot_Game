856dd6e471e57ec108c8678cffbc05171434aadd
Kamil <kamil.witonski@o2.pl>
2018-02-15 17:42:47 +0000

    init armour statictic

---
+++ robot/Assets/Scripts/ArmourStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourStats : MonoBehaviour {
+
+	public int weight;
+	public int protection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
