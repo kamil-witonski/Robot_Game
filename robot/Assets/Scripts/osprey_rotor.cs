6d003ebf1f9efa65c7519019c0ff7e69dd6527d2
Kamil <kamil.witonski@o2.pl>
2018-03-15 13:48:59 +0000

    add scripts for heli and osprey

---
+++ robot/Assets/Scripts/osprey_rotor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class osprey_rotor : MonoBehaviour {
+
+	public Transform rotor_l; 
+	public Transform rotor_r;
+
+	public bool engines_on = false;
+
+	//private Vector3 rotation = new Vector3(0f, 20f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (engines_on) {
+			
+			rotor_l.transform.Rotate (Vector3.forward * Time.deltaTime * 1500);
+			rotor_r.transform.Rotate (Vector3.forward * Time.deltaTime * 1500);
+		}
+	}
+}
